a8fad8b2e7376c433021f3b44bd9c4ac1cf66418
Francisco Criscuolo <fran0w@Franciscos-MacBook-Pro.local>
2020-02-11 13:58:30 -0300

    BackUp previo a sacar texturas pesadas.

---
+++ Assets/Scripts/Utilities/SettingBalance_handler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingBalance_handler : MonoBehaviour
+{
+    [SerializeField] private EntityConfig _playerSettings;
+    [SerializeField] private EntityConfig _playerSettings_BASE;
+
+    [SerializeField] private InputField _topSpeed;
+    [SerializeField] private InputField _overExcitedTopSpeed;
+
+    public void Reset()
+    {
+        _playerSettings.topSpeed = _playerSettings_BASE.topSpeed;
+        _playerSettings.overExcitedTopSpeed = _playerSettings_BASE.overExcitedTopSpeed;
+    }
+
+    public void SaveChanges()
+    {
+        if (_overExcitedTopSpeed.text == "" || _topSpeed.text == "")
+            return;
+        
+        _playerSettings.topSpeed = float.Parse( _topSpeed.text);
+        _playerSettings.overExcitedTopSpeed = float.Parse(_overExcitedTopSpeed.text);
+    }
+    
+    
+}
